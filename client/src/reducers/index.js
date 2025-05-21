5e57aea253b3a6d4265e90674dcb9d90c4aa09ce
Jose Eneas da Silva Maria <joseeneas@me.com>
2025-05-21 15:11:03 -0400

    Updating app documentation

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,3 +1,41 @@
+/*
+This code is written in JavaScript, specifically using Redux, which is a predictable state container for JavaScript apps. 
+Redux helps you write applications that behave consistently, run in different environments (client, server, and native), 
+and are easy to test. 
+It is commonly used with libraries like React or Angular for building user interfaces.
+
+The commented line of code is:  `// import all reducers and combine them into one`.
+
+This suggests that multiple reducers are being combined into one single root reducer.
+This is a common practice in Redux applications when you have different parts of state that are managed by different reducers.
+
+The next few lines of code are imports:
+```javascript
+import { combineReducers } from "redux";
+import posts from './posts.js'
+import auth from './auth.js'
+```
+Here, `{ combineReducers }` is imported from 'redux'.
+This function turns an object whose values are different reducing functions into a single reducing function. 
+The reducers `posts` and `auth` are imported from their respective files('./posts.js' and './auth.js').
+
+In Redux, 'reducers' specify how the application's state changes in response to actions. 
+For instance, a post reducer may update the number of likes on a post while the auth reducer may update the user's login status.
+
+The last few lines:
+```javascript
+export default combineReducers ({
+    posts,
+    auth
+})
+```
+combineReducers is called with an object that contains the `posts` and `auth` reducers.
+This will create a single root reducer out of them.
+Consequently, when an action is dispatched, `combineReducers` ensures that each reducer
+gets called and gets a chance to update its part of the state.
+Finally, it's exported as a default, meaning that other modules can import this combined reducer without knowing its name. 
+This is usually imported in the store setup where it is used to create a new Redux store.
+*/
 // import all reducers and combine them into one
 import { combineReducers } from "redux";
 // import reducers
@@ -5,7 +43,7 @@ import posts from './posts.js'
 import auth from './auth.js'
 // combine reducers
 // export the combined reducers
-export default combineReducers ({
+export default combineReducers({
     posts,
     auth
 })
