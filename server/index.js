f22c6491518f94006209968485382e5b47b52a4c
Jose Eneas da Silva Maria <joseeneas@me.com>
2025-08-02 18:34:52 -0400

    Commit

--- server/index.js
+++ server/index.js
@@ -69,5 +69,6 @@ const PORT = process.env.PORT || 8080;
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/instaverse", ({
 }))
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-    .catch(err => console.log(err.message));
+    .catch(err => console.log(err.message))
+console.log('Database connected ...');
 console.log('App started ...');
